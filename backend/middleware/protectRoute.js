fa37169737e8906e2dd1b037a9faa2c602051f41
nihalp19 <nihalpanday2020@gmail.com>
2024-12-23 23:53:18 +0530

    add middleware for authorization

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import USER from "../models/user-models"
+
+async function protectRoute(req,res,next){
+    try{
+        const token = req.cookies.token
+        if(!token){
+            return res.status(400).json({success : false,message : "User is not authenicated"})
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECERET_TOKEN)
+        if(!decoded){
+            return res.status(400).json({success : false,message : "Token is expired or invalid"})
+        }
+
+        const user = await USER.findById(decoded.userId)
+        if(!user){
+            return res.status(400).json({success : false,message : "User is not found"})
+        }
+        req.user = user
+        next()
+    }catch(error){
+        console.log("Error ",error.message)
+        return res.status(500).json({message : false,message : "Internal Server Error",error: error.message})
+    }
+}
